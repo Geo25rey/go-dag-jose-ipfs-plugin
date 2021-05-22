d276831f3d8458a174809857982791fd269398ff
Geoffrey Casper <gcasper42@gmail.com>
2021-05-22 11:13:02 -0400

    fix: input/format types

--- plugin.go
+++ plugin.go
@@ -1,95 +1,103 @@
 package example
 
 import (
+	"github.com/Geo25rey/go-dag-jose/dagjose"
+	blocks "github.com/ipfs/go-block-format"
+	"github.com/ipfs/go-ipfs/core/coredag"
 	"github.com/ipfs/go-ipfs/plugin"
-    "github.com/ipfs/go-ipfs/core/coredag"
-    ipld "github.com/ipfs/go-ipld-format"
-    legacy "github.com/ipfs/go-ipld-legacy"
-    dagcbor "github.com/ipld/go-ipld-prime/codec/dagcbor"
-    blocks "github.com/ipfs/go-block-format"
-    "github.com/Geo25rey/go-dag-jose/dagjose"
+	ipld "github.com/ipfs/go-ipld-format"
+	legacy "github.com/ipfs/go-ipld-legacy"
+	dagcbor "github.com/ipld/go-ipld-prime/codec/dagcbor"
 
-    "bytes"
-    "io"
-    "io/ioutil"
+	"bytes"
+	"io"
+	"io/ioutil"
 )
 
 type ipldDagJose struct{}
 
 // Plugins is an exported list of plugins that will be loaded by go-ipfs.
 var Plugins = []plugin.Plugin{
-    &ipldDagJose{},
+	&ipldDagJose{},
 }
 
 func decoder(block blocks.Block) (ipld.Node, error) {
-    nodeBuilder := dagjose.NewBuilder()
-    buf := bytes.NewBuffer(block.RawData())
-    err := dagcbor.Decode(nodeBuilder, buf)
-    if (err != nil) {
-        return nil, err
-    }
-    primeNode := nodeBuilder.Build()
-    return &legacy.LegacyNode{
-        block,
-        primeNode,
-    }, nil
+	nodeBuilder := dagjose.NewBuilder()
+	buf := bytes.NewBuffer(block.RawData())
+	err := dagcbor.Decode(nodeBuilder, buf)
+	if err != nil {
+		println("Failed to decode:", err)
+		return nil, err
+	}
+	primeNode := nodeBuilder.Build()
+	println("Decode success")
+	return &legacy.LegacyNode{
+		Block: block,
+		Node:  primeNode,
+	}, nil
 }
 
 func (dagJose *ipldDagJose) RegisterBlockDecoders(dec ipld.BlockDecoder) error {
-    dec.Register(0x85, decoder)
-    return nil
+	dec.Register(0x85, decoder)
+	return nil
 }
 
 func encoder(r io.Reader, mhType uint64, mhLen int) (result []ipld.Node, err error) {
-    if (mhType != 0x15 || mhLen != 48) {
-        return
-    }
-    buf, err := ioutil.ReadAll(r)
-    if (err != nil) {
-        return
-    }
-    nodeBuilder := dagjose.NewBuilder()
-    err = nodeBuilder.AssignBytes(buf)
-    if (err != nil) {
-        return
-    }
-    primeNode := nodeBuilder.Build()
-    block := blocks.NewBlock(buf)
-    legacyNode := &legacy.LegacyNode{
-        block,
-        primeNode,
-    }
-    err = nil
-    result = append(result, legacyNode)
-    return
+	if mhType != 0x15 || mhLen != 48 {
+		println("mhType=", mhType)
+		println("mhLen=", mhLen)
+		println("mhType or mhLen doesn't match")
+		return
+	}
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		println("Error from read:", err)
+		return
+	}
+	nodeBuilder := dagjose.NewBuilder()
+	err = nodeBuilder.AssignBytes(buf)
+	if err != nil {
+		println("Error from building:", err)
+		return
+	}
+	primeNode := nodeBuilder.Build()
+	block := blocks.NewBlock(buf)
+	legacyNode := &legacy.LegacyNode{
+		Block: block,
+		Node:  primeNode,
+	}
+	err = nil
+	result = []ipld.Node{legacyNode}
+	println("Successfully built node")
+	return
 }
 
 var (
-    inputEncodings []string = []string{
-        "dagjose",
-    }
-    formats []string = []string{
-        "dag-cbor",
-    }
+	inputEncodings []string = []string{
+		"raw",
+	}
+	formats []string = []string{
+		"dag-jose",
+	}
 )
 
 func (dagJose *ipldDagJose) RegisterInputEncParsers(iec coredag.InputEncParsers) error {
-    for _, inputEncoding := range inputEncodings {
-        for _, format := range formats {
-           iec.AddParser(inputEncoding, format, encoder)
-        }
-    }
-    return nil
+	for _, inputEncoding := range inputEncodings {
+		for _, format := range formats {
+			iec.AddParser(inputEncoding, format, encoder)
+		}
+	}
+	return nil
 }
 
 func (dagJose *ipldDagJose) Name() string {
-    return "ipldDagJose"
+	return "ipldDagJose"
 }
 
 func (dagJose *ipldDagJose) Version() string {
-    return "0.1.0"
+	return "0.1.0"
 }
 
 func (dagJose *ipldDagJose) Init(env *plugin.Environment) error {
-    return nil
+	return nil
 }
