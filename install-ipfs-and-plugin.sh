0fa618413d498054a53b9c1f5569258a66c7ad75
Geo25rey <gcasper42@gmail.com>
2023-12-14 11:07:43 -0500

    require pure go when building at cost of hashing performance

--- install-ipfs-and-plugin.sh
+++ install-ipfs-and-plugin.sh
@@ -8,7 +8,7 @@ go get "github.com/ipfs/go-ipfs/cmd/ipfs@${IPFS_VERSION}"
 # Build the IPFS plugin's shared library
 echo "Building the IPFS plugin"
 mkdir -p build
-go build -buildmode=plugin -asmflags=all=-trimpath=\"${GOPATH}\" -gcflags=all=-trimpath=\"${GOPATH}\" -o build/plugin.so ./main/
+go build -buildmode=plugin -asmflags=all=-trimpath=\"${GOPATH}\" -gcflags=all=-trimpath=\"${GOPATH}\" -tags purego -o build/plugin.so ./main/
 
 # Mark the shared library as executable
 echo "Marking the IPFS plugin as executable"
